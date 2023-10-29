458f37c966897b10afdea5f3447e3a8319821625
Francisco Frota de Aguiar <francisco.fro.agui@gmail.com>
2023-10-29 20:28:33 -0300

    criacao do projeto base

---
+++ ESR-INTRO/src/main/java/com/fran/ESRINTRO/EsrIntroApplication.java
@@ -0,0 +1,13 @@
+package com.fran.ESRINTRO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsrIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsrIntroApplication.class, args);
+	}
+
+}
---
+++ ESR-INTRO/src/test/java/com/fran/ESRINTRO/EsrIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fran.ESRINTRO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsrIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
